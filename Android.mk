58e2cb0a1b4a0638ed25f079cf63d5931d8d6aba
Polytheus <polytheus@googlemail.com>
2011-02-05 16:02:27 +0100

    Android.mk: include subdirs
    Change-Id: I675c1fea416e71e32b8deaf6a9b2173c080bc4f5

--- Android.mk
+++ Android.mk
@@ -17,3 +17,6 @@ $(KERNEL_CONFIG): $(LOCAL_PATH)/arch/arm/configs/openetna | $(ACP)
 
 $(TARGET_PREBUILT_KERNEL): $(KERNEL_OUT) $(KERNEL_CONFIG)
 	$(MAKE) -C kernel O=../$(KERNEL_OUT) ARCH=arm CROSS_COMPILE=$(shell pwd)/prebuilt/linux-x86/toolchain/arm-eabi-4.4.0/bin/arm-eabi- INSTALL_MOD_STRIP=1
+
+include $(all-subdir-makefiles)
+
